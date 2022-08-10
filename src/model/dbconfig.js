75c0facfaeb92f585cd1c6d6e74fed26ddeebc0d
FJORWB <88600252+fjorwb@users.noreply.github.com>
2022-08-10 12:55:36 -0500

    deploying in heroku

--- src/model/dbconfig.js
+++ src/model/dbconfig.js
@@ -2,12 +2,22 @@ require('dotenv').config()
 
 const pgp = require('pg-promise')()
 
+let ssl = null
+
+if (process.env.NODE_ENV === 'production') {
+  ssl = {
+    rejectUnauthorized: true
+  }
+}
+
 const connection = ({
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD
+  password: process.env.DB_PASSWORD,
+  max: 20,
+  ssl
 })
 
 const db = pgp(connection)
